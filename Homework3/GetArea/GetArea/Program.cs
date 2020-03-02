74bab1db4b62e578b3f72e698cbc8aa59e74156b
WeiChangxin <850974330@qq.com>
2020-03-02 16:30:42 +0800

    homework3

---
+++ Homework3/GetArea/GetArea/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace GetArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10.0,1.0);
+            r.getarea();
+            Square s = new Square(10.0);
+            s.getarea();
+            Triangle tri = new Triangle(3.0, 4.0, 5.0);
+            tri.getarea();
+        }
+
+        interface Area
+        {
+
+            bool judge();
+            void getarea();
+
+
+        }
+
+
+        class Rectangle : Area
+        {
+            double lline;
+            double sline;
+            double area;
+            public Rectangle(double a, double b)
+            {
+                lline = a;
+                sline = b;
+            }
+            
+            public bool judge()
+            {
+                return lline > 0 && sline > 0;
+                
+            }
+             public void getarea()
+            {
+                if (!judge())
+                {
+                    Console.WriteLine("该长方形不合法！");
+                    return;
+                }
+                area = lline * sline;
+                Console.WriteLine("该长方形的面积为：" + area);
+
+            }
+    
+        }
+       
+        class Square:Area
+        {
+            double line;
+            double area;
+            public Square (double a)
+            {
+                line = a;
+            }
+            public bool judge()
+            {
+                return line > 0;
+                
+            }
+            public void getarea()
+            {
+                if (!judge())
+                {
+                    Console.WriteLine("该正方形形状不合法！");
+                    return;
+                }
+                area = line * line;
+                Console.WriteLine("该正方形的面积为：" + area);
+            }
+
+        }
+        class Triangle:Area
+        {
+            double aline;
+            double bline;
+            double cline;
+            double high;
+            public Triangle (double a,double b,double c)
+            {
+                aline = a;
+                bline = b;
+                cline = c;
+            }
+            public bool judge()
+            {
+                return aline > 0 && bline > 0&&cline>0&&aline+bline>cline&&aline+cline>bline&&bline+cline>aline;
+               
+            }
+            public void getarea()
+            {
+                if (!judge())
+                {
+                    Console.WriteLine("该三角形不合法！");
+                    return;
+                }
+                double p = (aline + bline + cline) / 2;
+                double area = Math.Sqrt(p * (p - aline) * (p - bline) * (p - cline));
+                Console.WriteLine("该三角形的面积为:" + area);
+            }
+        }
+     
+
+    }
+}
---
+++ Homework3/ShapeFactory/ShapeFactory/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace ShapeFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory rectangleFactory = new RectangleFactory();
+            ShapeFactory squareFactory = new SquareFactory();
+            ShapeFactory tritangleFactory = new TritangleFactory();
+            int allarea = 0;
+            for (int i = 0; i < 10; i++)
+            {
+               
+                Random random = new Random(); //在外面生成对象
+                int r = random.Next(1, 4);
+                switch (r)
+                {
+                    case 1:
+                        
+                        allarea+=rectangleFactory.createShape().getarea();
+                        Console.WriteLine("产生了一个长方形，面积为：{0}", rectangleFactory.createShape().getarea());
+                        break;
+                    case 2:
+                        allarea += squareFactory.createShape().getarea();
+                        Console.WriteLine("产生了一个正方形，面积为：{0}", squareFactory.createShape().getarea());
+                        break;
+                    case 3:
+                        allarea += tritangleFactory.createShape().getarea();
+                        Console.WriteLine("产生了一个三角形，面积为：{0}", tritangleFactory.createShape().getarea());
+                        break;
+
+
+                }
+                
+            }
+            Console.WriteLine("十个形状总面积为：{0}", allarea);
+
+        }
+        
+
+        public abstract class Shape
+        {
+          
+            public abstract int getarea();
+        }
+        public class Rectangle : Shape
+        {
+            
+            public override int getarea()
+            {
+                Random random = new Random(); //在外面生成对象
+                int longline = random.Next(1, 10);
+                int shortline = random.Next(1, 10);
+                int area = longline * shortline;
+               return area;
+                throw new NotImplementedException();
+            }
+        }
+        public class Square : Shape
+        {
+
+            public override int getarea()
+            {
+                Random random = new Random(); //在外面生成对象
+                int line = random.Next(1, 10);
+               
+                int area = line * line;
+
+                return area;
+                throw new NotImplementedException();
+
+            }
+        }
+         public class Tritangle : Shape
+        {
+            
+            public override int getarea()
+            {
+                Random random = new Random(); //在外面生成对象
+                int aline = random.Next(6, 10);
+                int bline = random.Next(6, 10);
+                int cline = random.Next(6, 10);
+                double p = (aline +bline + cline )/ 2;
+                double area = Math.Sqrt(p * (p - aline) * (p - bline) * (p - cline));
+                return (int)area;
+                throw new NotImplementedException();
+            }
+        }
+        public abstract class ShapeFactory
+        {
+            public abstract Shape createShape();
+        }
+        public class RectangleFactory : ShapeFactory
+        {
+            public override Shape createShape()
+            {
+                return new Rectangle();
+                throw new NotImplementedException();
+            }
+        }
+        public class SquareFactory : ShapeFactory
+        {
+            public override Shape createShape()
+            {
+                return new Square();
+                throw new NotImplementedException();
+            }
+        }
+        public class TritangleFactory : ShapeFactory
+        {
+            public override Shape createShape()
+            {
+                return new Tritangle();
+                throw new NotImplementedException();
+            }
+        }
+
+
+
+
+    }
+}
